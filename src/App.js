3d55bd7a97c3cfd95c2109e842fe093575a012b8
BirrellC <vaiezmedia@gmail.com>
2022-02-15 20:05:57 +0000

    Feat: create calculator buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const App = () => {
-  return <div> App </div>;
+  const createNumbers = () => {
+    const numbers = [];
+
+    for (let i = 1; i < 10; i++) {
+      numbers.push(<button key={i}>{i}</button>);
+    }
+    return numbers;
+  };
+
+  return (
+    <div className='App'>
+      <div className='calculator'>
+        <div className='display'>0</div>
+
+        <div className='operands'>
+          <button>÷</button>
+          <button>x</button>
+          <button>-</button>
+          <button>+</button>
+          <button>C</button>
+        </div>
+
+        <div className='keys'>
+          {createNumbers()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
